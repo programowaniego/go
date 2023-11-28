84f773b6a7d432cea6ccb8073c75c70639fd8475
programowaniego <151568070+programowaniego@users.noreply.github.com>
2023-11-28 13:22:26 +0100

    Update wpisywanie.c

--- wpisywanie.c
+++ wpisywanie.c
@@ -1,5 +1,5 @@
 #ifndef wpisywanie.c
-int pozycja(char kolumna , char wiersz)
+int pozycja()
 {
 int pozycja = 0;
 printf("podaj pole na ktorym chcesz postawic pionek (kolumna , wiersz)(np A1)\n");  
