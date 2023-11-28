5b3851e14f7ec01bf13721a880058ca3afd2e144
programowaniego <151568070+programowaniego@users.noreply.github.com>
2023-11-28 13:08:05 +0100

    Add files via upload

---
+++ wpisywanie.c
@@ -0,0 +1,18 @@
+#ifndef wpisywanie.c
+int konwersja(char kolumna , char wiersz)
+{
+int pozycja = 0;
+printf("podaj pole na ktorym chcesz postawic pionek (kolumna , wiersz)(np A1)\n");  
+scanf("%c%d", &kolumna , &wiersz);
+getchar();
+ while(kolumna < 'A'|| kolumna  > 'M' || wiersz < 0 || wiersz > 13)
+    {
+        printf("podano niewlasciwa wartosc kolumny/wiersza\n");
+        printf("podaj pole na ktorym chcesz postawic pionek (kolumna , wiersz)(np A1)\n");  
+        scanf("%c%d", &kolumna , &wiersz);
+        getchar();
+    }
+    pozycja = (wiersz - 1)*13 + kolumna - 'A';
+    return pozycja;
+}
+#endif
