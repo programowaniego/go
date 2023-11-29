8856f82441b044e3e95dd12333e8601e5d5e34dc
programowaniego <151568070+programowaniego@users.noreply.github.com>
2023-11-29 12:21:36 +0100

    Poprawna wersja

--- wpisywanie.c
+++ wpisywanie.c
@@ -2,19 +2,19 @@
 int pozycja()
 {
 int pozycja = 0;
-int kolumna;
-char wiersz;
+ char kolumna;
+ int wiersz;
 printf("podaj pole na ktorym chcesz postawic pionek (kolumna , wiersz)(np A1)\n");  
-scanf("%c%d", &wiersz, &kolumna );
+scanf("%c%d", &kolumna , &wiersz);
 getchar();
-while(wiersz < 'A'|| wiersz  > 'M' || kolumna < 0 || kolumna > 13)
-{
-    printf("podano niewlasciwa wartosc kolumny/wiersza\n");
-    printf("podaj pole na ktorym chcesz postawic pionek (kolumna , wiersz)(np A1)\n");  
-    scanf("%c%d", &wiersz , &kolumna);
-    getchar();
-}
-pozycja = (wiersz - 'A')*13 + kolumna - 1;
-return pozycja;
+ while(kolumna < 'A'|| kolumna  > 'M' || wiersz < 0 || wiersz > 13)
+    {
+        printf("podano niewlasciwa wartosc kolumny/wiersza\n");
+        printf("podaj pole na ktorym chcesz postawic pionek (kolumna , wiersz)(np A1)\n");  
+        scanf("%c%d", &kolumna , &wiersz);
+        getchar();
+    }
+    pozycja = (wiersz - 1)*13 + kolumna - 'A';
+    return pozycja;
 }
 #endif
